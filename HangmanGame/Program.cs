21d35b87ed98ef79afe0860df1f42aa7e56b6b08
zzzxx729 <vvvc40.v4@gmail.com>
2023-10-17 21:07:01 +0300

    New change

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Hangman!");
+        Console.WriteLine("-------------------");
+        Console.WriteLine("Press any key to start");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
